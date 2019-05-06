1759433dead73d2fd53643b7442b889707f1e15e
YasinZhang <271155397@qq.com>
2019-05-06 19:27:44 +0800

    sync

--- src/main/java/com/yasinmall/configuration/WebConfig.java
+++ src/main/java/com/yasinmall/configuration/WebConfig.java
@@ -1,12 +1,14 @@
 package com.yasinmall.configuration;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 
 /**
  * @author Yasin Zhang
  */
+@Configuration
 public class WebConfig {
 
     //配置文件上传
